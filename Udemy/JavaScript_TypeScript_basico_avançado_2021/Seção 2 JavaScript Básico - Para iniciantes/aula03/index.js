e686c5316cdddf8c1f77dd13ec68a981b9532fa0
Rodrigo de Souza <83836552+RodrigoSouzaSilva@users.noreply.github.com>
2021-09-14 11:31:46 -0300

    criando aula03

---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Seção 2 JavaScript Básico - Para iniciantes/aula03/index.js
@@ -0,0 +1,7 @@
+console.log("Rodrigo de 'Souza'"); 
+console.log('Rodrigo de "Souza"'); 
+console.log(`Rodrigo de Souza`);   
+console.log(123456, 15.85, 'Rodrigo de Souza');              
+
+
+// ponto e virgula e opcional
