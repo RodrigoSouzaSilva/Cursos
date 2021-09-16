a3d439ce928187438b6ae0b7381fc551d4f3e1e7
Rodrigo de Souza <83836552+RodrigoSouzaSilva@users.noreply.github.com>
2021-09-16 16:33:38 -0300

    aula 13, 14 e anotações

---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Seção 2 JavaScript Básico - Para iniciantes/aula14/index.js
@@ -0,0 +1,29 @@
+//IEEE 754-2008
+let num1 = 0.7   // number
+let num2 = 0.1   // number
+
+num1 += num2
+num1 += num2
+num1 += num2
+
+num1 = num1.toFixed(2)
+
+console.log(num1);
+
+//num1 = num1.toString()
+//passando number para string
+
+// console.log(num1.toString(2));
+// //transformando em número binário
+
+// console.log(num1.toFixed(3));
+// //colocando quantas casas decimais irá querer e faz o arredondamento
+
+// console.log(Number.isInteger(num1));
+// //verifica se o número e inteiro
+
+// let temp = num1 * 'Olá'
+// console.log(Number.isNaN(temp));
+// //verifica se o valor retornado e um NaN
+
+
