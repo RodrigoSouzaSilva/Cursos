84db28fe0132ae80fc30e6ac9dabc21608610103
Rodrigo de Souza <83836552+RodrigoSouzaSilva@users.noreply.github.com>
2021-09-28 13:52:31 -0300

    criando aula 48 e atualizando anotações 03

---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Seção 3 JavaScript - Lógica de programação/aula48_exercicio/assets/js/main.js
@@ -0,0 +1,78 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi(){
+    const li = document.createElement('li');
+    return li; //li desta função, diferente do outro
+}
+
+inputTarefa.addEventListener('keypress', function(e){ //vigiando a tecla ENTer
+    if(e.keyCode === 13){
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus(); //para deixar o focus do ponteiro marcado nela
+}
+
+function criaBotaoApagar(li){
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    // botaoApagar.classList.add('apagar');
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar está tarefa');
+    li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput){
+    const li = criaLi();  //li desta função, diferente o outro
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+btnTarefa.addEventListener('click', function(){
+    if (!inputTarefa.value) return; //se for vazio nada acontece
+    criaTarefa(inputTarefa.value); //joga o texto pra outra função
+});
+
+document.addEventListener('click', function(e){
+    const el = e.target;
+    
+    if (el.classList.contains('apagar')){
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+    
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas();
