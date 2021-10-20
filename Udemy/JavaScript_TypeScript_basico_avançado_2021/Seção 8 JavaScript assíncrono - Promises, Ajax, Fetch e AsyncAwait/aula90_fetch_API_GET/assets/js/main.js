c7600b43448e850d4791fac9a83cb952b71191d0
Rodrigo de Souza <83836552+RodrigoSouzaSilva@users.noreply.github.com>
2021-10-20 18:20:00 -0300

    criando aula 89, 90, 91 e atulizando anotações 08

---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Seção 8 JavaScript assíncrono - Promises, Ajax, Fetch e AsyncAwait/aula90_fetch_API_GET/assets/js/main.js
@@ -0,0 +1,38 @@
+// essa fetch, já retorna por padrão uma promisse
+fetch('pagina1.html')
+.then(resposta => {
+  if(resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+  return resposta.text();
+})
+.then(html => console.log(html))
+.catch(e => console.log(e)); 
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute('href');
+    const response = await fetch(href);;
+
+    if(response.status !== 200) throw new Error('ERRO!!!');
+
+    const html = await response.text();
+    carregaResultado(html);
+  } catch(e) {
+    console.log(e);
+  }
+  
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
