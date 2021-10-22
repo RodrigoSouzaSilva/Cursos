6008af659a69897ab54f78b5d973660942a49095
Rodrigo de Souza <83836552+RodrigoSouzaSilva@users.noreply.github.com>
2021-10-22 11:45:46 -0300

    criando aula 94 e atualizando anotações

---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Seção 9 Javascript Tooling e ES6 Modules - Módulos/aula94_ES6_modules_Import_Export/src/index.js
@@ -0,0 +1,4 @@
+import padrao, {nome, sobrenome, idade, soma} from './modulo1'
+
+console.log(padrao(5, 40));
+console.log(nome, sobrenome, idade, soma(3,9));
---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Seção 9 Javascript Tooling e ES6 Modules - Módulos/aula94_ES6_modules_Import_Export/src/modulo1.js
@@ -0,0 +1,12 @@
+const nome = 'Luiz';
+const sobrenome = 'Miranda';
+const idade = 30;
+const cpf = 33311122;
+
+function soma(x, y) {
+    return x + y;
+}
+
+export default (x, y) => x * y;
+
+export {nome, sobrenome, idade, soma}
