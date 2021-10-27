93769579f334b4aa7c8dbd9492a31ad4d43498b4
RodrigoSouzaSilva <rodrigo_souza.91@hotmail.com>
2021-10-27 08:17:46 -0300

    atualizando pasta raiz

---
+++ Udemy/JavaScript_TypeScript_basico_avançado_2021/Secao_18_TypeScript-Tipos_basicos/src/03-tipo-void/tipo-void.ts
@@ -0,0 +1,18 @@
+function semRetorno(...args:string[]){
+    console.log(args.join(' + '));
+}
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otavio',
+
+
+    exibirNome():void {
+        console.log(this.nome + ' ' +this.sobrenome)
+    }
+}
+
+semRetorno('Luiz', 'Otavio')
+pessoa.exibirNome()
+
+export { pessoa }
